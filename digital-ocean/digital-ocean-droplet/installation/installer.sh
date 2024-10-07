80eed6d486eba38a9ac8c60e16e79f6a05e04403
Vikash Badal <vikashb@where-ever.za.net>
2024-10-07 09:10:28 +0200

    rename group_name to environment update readme make droplet count and size variable

--- digital-ocean/digital-ocean-droplet/installation/installer.sh
+++ digital-ocean/digital-ocean-droplet/installation/installer.sh
@@ -22,17 +22,17 @@ apt install -yq \
     containerd.io \
     docker-buildx-plugin \
     docker-compose-plugin \
-    apparmor-utils
+    apparmor-utils \
+    sshguard
 
 curl -sL https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker \
     -o /etc/bash_completion.d/docker.sh
 
 # Enable and configure firewall
-
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow OpenSSH
-sudo ufw --force enable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow OpenSSH
+ufw --force enable
 
 # Add useraccounts
 for ADM in vikash.badal donovan.austin raghuveersingh.choudhari sparsh.sharma
@@ -45,3 +45,21 @@ do
         && chmod 644 /home/${ADM}/.ssh/authorized_keys
     chown -R ${ADM} /home/${ADM}/.ssh
 done
+
+# Configure ufw for sshguard
+LINES=$(cat /etc/ufw/before.rules | wc -l)
+NEEDED=$((LINES-2))
+head -n ${NEEDED} /etc/ufw/before.rules > /tmp/ufw-before.rules
+
+cat <<EOT >> /tmp/ufw-before.rules
+# for ipv4
+:sshguard - [0:0]
+-A ufw-before-input -p tcp --dport 22 -j sshguard
+# for ipv6
+:sshguard - [0:0]
+-A ufw6-before-input -p tcp --dport 22 -j sshguard
+
+# don't delete the 'COMMIT' line or these rules won't be processed
+COMMIT
+EOT
+reboot
--- digital-ocean/digital-ocean-droplet/variables.tf
+++ digital-ocean/digital-ocean-droplet/variables.tf
@@ -1,7 +1,14 @@
 variable "do_token" {}
-#variable "private_key" {}
-variable "group_name" {}
+variable "droplet_count" {
+  type = number
+  default = 1
+}
+variable "droplet_size" {
+  type = string
+  default = "s-1vcpu-1gb"
+}
+variable "environment" {}
 variable "droplet_names" {
-   type    = list(string)
-   default = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  type    = list(string)
+  default = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 }

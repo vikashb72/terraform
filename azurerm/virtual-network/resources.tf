c259cf7028d1428eb2fce7e76fdcfb709fc75d50
Vikash Badal <vikashb@where-ever.za.net>
2024-10-08 15:43:14 +0200

    update variables add outputs

--- azurerm/virtual-network/resources.tf
+++ azurerm/virtual-network/resources.tf
@@ -1,6 +1,56 @@
+# Create Management virtual network 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.environment}-home.where-ever"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.vnet_subnets
   location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
 }
+
+# Create Gateway Subnet
+resource "azurerm_subnet" "snet_gateway" {
+  name                 = "snet-gateway-${var.environment}-home.where-ever"
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.gateway_subnet
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
+
+# Create bastion host subnet
+#resource "azurerm_subnet" "snet_bastion" {
+#  name                                          = "snet-bastion-${var.environment}-home.where-ever"
+#  resource_group_name                           = azurerm_virtual_network.vnet.resource_group_name
+#  virtual_network_name                          = azurerm_virtual_network.vnet.name
+#  address_prefixes                              = var.bastion_subnet
+#  private_endpoint_network_policies_enabled     = false
+#  private_link_service_network_policies_enabled = false
+#  depends_on = [
+#    azurerm_virtual_network.vnet
+#  ]
+#}
+#
+# Create firewall subnet
+resource "azurerm_subnet" "snet_firewall" {
+#  name                                          = "snet-firewall-${var.environment}-home.where-ever"
+#  resource_group_name                           = azurerm_virtual_network.vnet.resource_group_name
+#  virtual_network_name                          = azurerm_virtual_network.vnet.name
+#  address_prefixes                              = var.firewall_subnet
+#  private_endpoint_network_policies_enabled     = false
+#  private_link_service_network_policies_enabled = false
+#  depends_on = [
+#    azurerm_virtual_network.vnet
+#  ]
+#}
+# Create psql subnet
+resource "azurerm_subnet" "snet_psql" {
+#  name                                          = "snet-psql-${var.environment}-home.where-ever"
+#  resource_group_name                           = azurerm_virtual_network.vnet.resource_group_name
+#  virtual_network_name                          = azurerm_virtual_network.vnet.name
+#  address_prefixes                              = var.firewall_subnet
+#  private_endpoint_network_policies_enabled     = false
+#  private_link_service_network_policies_enabled = false
+#  depends_on = [
+#    azurerm_virtual_network.vnet
+#  ]
+#}
--- azurerm/virtual-network/variables.tf
+++ azurerm/virtual-network/variables.tf
@@ -1,2 +1,18 @@
 variable "environment" {}
 variable "az_tenant_id" {}
+variable "vnet_subnets" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+variable "gateway_subnet" {
+  type    = string
+  default = "10.0.0.0/28"
+}
+variable "bastion_subnet" {
+  type    = string
+  default = "10.0.0.16/28"
+}
+variable "firewall_subnet" {
+  type    = string
+  default = "10.0.0.32/28"
+}

300af5152d25bc77a7ce0d7af96e1f5dd9d80de1
Vikash Badal <vikashb@where-ever.za.net>
2024-11-05 15:07:15 +0200

    clean up

--- azurerm/base.old/backend.tf
+++
@@ -1,8 +0,0 @@
-#terraform {
-#  backend "azurerm" {
-#    # Variables may not be used here :(
-#    storage_account_name = "sa-${var.environment}-${var.suffix}"
-#    container_name       = "terraformstate"
-#    key                  = "terraform.tfstate"
-#  }
-#}
--- azurerm/base.old/resource-rg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = "rg-${var.environment}-${var.suffix}"
-  location = var.az_location
-}

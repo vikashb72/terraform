f8e7ea9b03115a7fbaa33b68341f737856656768
Vikash Badal <vikashb@where-ever.za.net>
2024-11-07 10:01:33 +0200

    fix storage_account.name (limited to 24 chars num + lower case only) fix storage_blob_record.name

--- azurerm/base/resource-storage-accounts.tf
+++ azurerm/base/resource-storage-accounts.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.environment}storageaccount"
+  name                     = "sa0whereever0${var.environment}"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
@@ -37,7 +37,8 @@ resource "azurerm_private_dns_zone_virtual_network_link" "storage_link_dns_zone_
 
 # Create a DNS Record for the Private Endpoint
 resource "azurerm_private_dns_a_record" "storage_blob_record" {
-  name                = azurerm_storage_account.storage_account.primary_blob_endpoint
+  #name                = azurerm_storage_account.storage_account.primary_blob_endpoint
+  name                = "storage-pvt-dns-${var.environment}"
   zone_name           = azurerm_private_dns_zone.pvt_dns_zone.name
   resource_group_name = azurerm_resource_group.resource_group.name
   ttl                 = 300

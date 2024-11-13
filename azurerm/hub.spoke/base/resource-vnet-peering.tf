31c0ba270fb069a0bc144c1e2ca04bca00b0b248
Vikash Badal <vikashb@where-ever.za.net>
2024-11-13 07:37:44 +0200

    fix duplication

--- azurerm/hub.spoke/base/resource-vnet-peering.tf
+++ azurerm/hub.spoke/base/resource-vnet-peering.tf
@@ -18,8 +18,8 @@ resource "azurerm_virtual_network_peering" "hubtoenv" {
 resource "azurerm_virtual_network_peering" "envtohub" {
   name                      = "peering-env-hub-${local.env.suffix}"
   resource_group_name       = azurerm_resource_group.resource_group.name
-  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
-  remote_virtual_network_id = azurerm_virtual_network.env_vnet.id
+  virtual_network_name      = azurerm_virtual_network.env_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
 
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
